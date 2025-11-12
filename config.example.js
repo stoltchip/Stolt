58e5719c766b8245b825bd8188cd060035fa70e5
stoltchip <kabanekna102@gmail.com>
2025-11-12 13:49:06 +0100

    Update and rename config.example.js to config.js

--- config.example.js
+++
@@ -1,3 +0,0 @@
-// Kopieer tenminste dit bestand naar 'config.js' en vul jouw Supabase gegevens in.
-export const SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
---
+++ config.js
@@ -0,0 +1,3 @@
+// Kopieer tenminste dit bestand naar 'config.js' en vul jouw Supabase gegevens in.
+export const SUPABASE_URL = "https://rkuzmwmtxlwriuhumynb.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrdXptd210eGx3cml1aHVteW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzcwNjIsImV4cCI6MjA3ODUxMzA2Mn0.E0NJZfXSZHOpg2JYWfywjNjbu5vs46Y0_aIgIKFxEGk";
