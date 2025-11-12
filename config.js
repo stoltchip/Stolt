48ac36e809920531bc4f4c683b41b16490328c7a
stoltchip <kabanekna102@gmail.com>
2025-11-12 12:40:17 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.SUPABASE_CONFIG = {
-  url: "https://ywckariplpewztjvqfhh.supabase.co", // ← wpisz tutaj swój Project URL z Supabase (początek: https://ywc...supabase.co)
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3Y2thcmlwbHBld3p0anZxZmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MDQyNzAsImV4cCI6MjA3ODQ4MDI3MH0.5-guB-0UwOqoTmKDcUeqJv55FT7juJf38gCWWWY7LDE",
-  storageBucket: "product-images"
-};
